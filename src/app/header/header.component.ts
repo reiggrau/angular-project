b1d43f69ee2b8dd84bf3b21cffb57f3fd066553c
reiggrau <guillemreiggrau@gmail.com>
2024-10-14 17:48:21 +0200

    Saving Ubuntu progress

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/>
+      <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true"/>
     </header>
   `,
   styleUrl: './header.component.css'
